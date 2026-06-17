07eea02c8d8380efdf8bf9891c71f830074892af
ChrisRodM12 <criromes@hotmail.com@hotmail.com>
2026-06-17 09:40:16 -0500

    feat: estructura inicial del portafolio con plantilla Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  
+  site: 'https://christianrodriguez6654-cloud.github.io',
+  
   vite: {
       plugins: [tailwindcss()],
   },
